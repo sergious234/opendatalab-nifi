0e3bd77a863c4e2982689c3ddd97833a5724ca41
sergio <sergious234@gmail.com>
2026-04-20 20:11:16 +0200

    Update Groovy scripts for nifi

--- data/nifi/groovy/MongoConnection.groovy
+++ data/nifi/groovy/MongoConnection.groovy
@@ -2,16 +2,16 @@ import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 
 class MongoConnection {
-    private static final String password = "\$ergio13~"
-    private static final String username = "sergio"
-    private static final String uri = String.format("mongodb://%s:%s@localhost:27017", username, password);
+		private static final String username = "root"
+		private static final String password = "example"
+		private static final String uri = String.format("mongodb://%s:%s@mongodb:27017", username, password);
 
-    static Optional<MongoClient> getMongoClient() {
-        try {
-            MongoClient mongoClient = MongoClients.create(uri);
-            return Optional.ofNullable(mongoClient);
-        } catch (Exception ignored) {
-            return Optional.empty()
-        }
-    }
+	static Optional<MongoClient> getMongoClient() {
+		try {
+			MongoClient mongoClient = MongoClients.create(uri);
+			return Optional.ofNullable(mongoClient);
+		} catch (Exception ignored) {
+			return Optional.empty()
+		}
+	}
 }
